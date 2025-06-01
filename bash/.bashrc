8faed2b08a65a39317ab08ff5e5f14a38545c1ff
RaghulSub <raghulsubramanian10@gmail.com>
2025-06-01 17:55:25 +0530

    added lazy loading for minconda

--- bash/.bashrc
+++ bash/.bashrc
@@ -11,18 +11,11 @@ export PS1='\[\e[0;32m\]\u@\h \[\e[0;34m\]\w\[\e[0m\]/ $ '
 
 
 # >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-#__conda_setup="$('/home/raghul/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-#if [ $? -eq 0 ]; then
-#    eval "$__conda_setup"
-#else
-#    if [ -f "/home/raghul/miniconda3/etc/profile.d/conda.sh" ]; then
-#        . "/home/raghul/miniconda3/etc/profile.d/conda.sh"
-#    else
-#        export PATH="/home/raghul/miniconda3/bin:$PATH"
-#    fi
-#fi
-#unset __conda_setup
+
+if [ -f "$HOME/.dotfiles/bash/scripts/miniconda.sh" ]; then
+    . "$HOME/.dotfiles/bash/scripts/miniconda.sh"
+fi
+
 # <<< conda initialize <<<
 
 source /usr/share/nvm/init-nvm.sh
--- bash/scripts/miniconda.sh
+++ bash/scripts/miniconda.sh
@@ -1,16 +1,25 @@
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if command -v /home/raghul/miniconda3/bin/conda &> /dev/null; then
-    __conda_setup="$('/home/raghul/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-    if [ $? -eq 0 ]; then
-        eval "$__conda_setup"
-    else
-        if [ -f "/home/raghul/miniconda3/etc/profile.d/conda.sh" ]; then
-            . "/home/raghul/miniconda3/etc/profile.d/conda.sh"
+
+_conda_lazy_load(){
+    if command -v /home/raghul/miniconda3/bin/conda &> /dev/null; then
+        unset -f conda
+        __conda_setup="$('/home/raghul/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+        if [ $? -eq 0 ]; then
+            eval "$__conda_setup"
         else
-            export PATH="/home/raghul/miniconda3/bin:$PATH"
+            if [ -f "/home/raghul/miniconda3/etc/profile.d/conda.sh" ]; then
+                . "/home/raghul/miniconda3/etc/profile.d/conda.sh"
+            else
+                export PATH="/home/raghul/miniconda3/bin:$PATH"
+            fi
         fi
+        unset __conda_setup
+        conda "$@"
     fi
-    unset __conda_setup
-fi
+}
+
+conda () {
+    _conda_lazy_load "$@"
+}
 # <<< conda initialize <<<

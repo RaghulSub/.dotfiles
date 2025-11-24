999c3390d8b8b0b5b4b51f35832b0e581abfa0ce
RaghulSub <raghulsubramanian10@gmail.com>
2025-11-24 22:46:13 +0530

    feat: added some new plugins

---
+++ nvim/lua/raghul/plugins/center.lua
@@ -0,0 +1,42 @@
+
+local dependencies = {}
+
+local init =  function () end
+
+local opts = {
+    skip_filetypes = {},
+    enabled = true,
+    allow_scroll_move = true,
+    disable_on_move = true,
+}
+
+local keys = {
+    {
+        "<leader>tc",
+        mode = { "n", "x" },
+        function() 
+            local stay_centered = require("stay-centered")
+            stay_centered.toggle()
+        end,
+        noremap = true,
+        silent = true,
+        desc = "toggle stay-centered.nvim",
+    },
+}
+
+return {
+    "arnamak/stay-centered.nvim",
+    version = "*",
+    enabled = true,
+    lazy = true,
+    event = {
+        "VeryLazy", 
+    },
+    cmd = {},
+    ft = {},
+    build = {},
+    dependencies = dependencies,
+    init = init,
+    opts = opts,
+    keys = keys,
+}
